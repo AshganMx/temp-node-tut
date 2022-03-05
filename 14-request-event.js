57865170971ec58c7608436010dcaf18f83a49c2
Josue Hdez <pj3mx1984@gmail.com>
2022-03-05 15:06:25 -0600

    Events documentation fix

--- 14-request-event.js
+++ 14-request-event.js
@@ -6,7 +6,9 @@ const http = require('http')
 
 // Using event emitter API
 const server = http.createServer()
-    // emits request event
+
+// emits request event
+// subscribe to it / listen for it / respond to it
 server.on('request', (req, res) => {
     res.end('Welcome')
 })
